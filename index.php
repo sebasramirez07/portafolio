d54e013ba79f4322571690322be41471b4d17257
sebasramirez07 <120290138+sebasramirez07@users.noreply.github.com>
2023-04-08 18:57:32 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Portafolio</title>
+</head>
+
+<body>
+    <header id="home" class="header">
+        <div>
+            <h1>MI PORTAFOLIO</h1>
+        </div>
+        <div class="p1">
+            <p>Hola soy estudiate de programacion</p>
+        </div>
+    </header>
+    <nav class="nav">
+        <div class="menu">
+            <a href="#home">inicio</a>
+            <a href="#lenguajes">Lenguajes</a>
+            <a href="#proyectos">Proyectos</a>
+            <a href="#Contacto">Contacto</a>
+        </div>
+    </nav>
+    <main class="main">
+        <div id="lenguajes" class="lenguajes">
+            <!-- primer bloque -->
+            <div class="row">
+                <div class="column">
+                    <div class="flip-box">
+                        <div class="flip-box-inner">
+                            <div class="flip-box-front">
+                                <h2>HTML</h2>
+                                <img class="imghtml" src="img/HTML5_logo_and_wordmark.svg-removebg-preview.png" alt="html">
+                            </div>
+                            <div class="flip-box-back">
+                                <ul>
+                                    <li>html5</li>
+                                    <li>paginas escalables</li>
+                                    <li>manejo de div</li>
+                                    <li>google font</li>
+                                    <li>imagenes</li>
+                                    <li>identacion</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="skills html">80%</div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="flip-box">
+                        <div class="flip-box-inner">
+                            <div class="flip-box-front">
+                                <h2>CSS</h2>
+                                <img class="imgcss" src="img//png-clipart-css3-cascading-style-sheets-logo-html-world-wide-web-blue-angle-thumbnail-removebg-preview.png" alt="css">
+
+                            </div>
+                            <div class="flip-box-back">
+                                <ul>
+                                    <li>imagenes</li>
+                                    <li>darle color a la pagina</li>
+                                    <li>maquetacion basica</li>
+                                    <li>margin</li>
+                                    <li>padding</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="skills css">60%</div>
+                    </div>
+                </div>
+            </div>
+            <!-- segundo bloque-->
+            <div class="row">
+                <div class="column">
+                    <div class="flip-box">
+                        <div class="flip-box-inner">
+                            <div class="flip-box-front">
+                                <h2>PHP</h2>
+                                <img class="imgphp" src="img/PHP-logo.svg-removebg-preview.png" alt="php">
+
+                            </div>
+                            <div class="flip-box-back">
+                                <ul>
+                                    <li>variables</li>
+                                    <li>aprendiendo modelo MVC</li>
+                                    <li>conexion a BD (aprendiendo)</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="skills php">30%</div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="flip-box">
+                        <div class="flip-box-inner">
+                            <div class="flip-box-front">
+                                <h2>JAVA</h2>
+                                <img class="imgjava" src="img/1366_2000-removebg-preview.png" alt="java">
+
+                            </div>
+                            <div class="flip-box-back">
+                                <ul>
+                                    <li>variables</li>
+                                    <li>solicitar datos (consola)</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="skills java">10%</div>
+                    </div>
+                </div>
+            </div>
+            <!-- tercer bloque -->
+            <div class="row">
+                <div class="column">
+                    <div class="flip-box">
+                        <div class="flip-box-inner">
+                            <div class="flip-box-front">
+                                <h2>C#</h2>
+                                <img class="imgcsharp" src="img//csharp.png" alt="csharp">
+
+                            </div>
+                            <div class="flip-box-back">
+                                <ul>
+                                    <li>aplicaciones por consola</li>
+                                    <li>windows form (basico)</li>
+                                    <li>conexion y CRUD (aprendiendo)</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="skills csharp">50%</div>
+                    </div>
+                </div>
+                <div class="column">
+                    <div class="flip-box">
+                        <div class="flip-box-inner">
+                            <div class="flip-box-front">
+                                <h2>MYSQL</h2>
+                                <img class="imgsql" src="img//mysql.png" alt="sql">
+
+                            </div>
+                            <div class="flip-box-back">
+                                <ul>
+                                    <li>crear base de datos</li>
+                                    <li>crear tablas</li>
+                                    <li>creacion de BD por Consola</li>
+                                    <li>Ingresar datos a la tabla</li>
+                                    <li>crear BD relacionales</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="container">
+                        <div class="skills mysql">90%</div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="proyectos" class="proyectos">
+                <div class="project">
+                    <h2 class="proyect">Proyectos</h2>
+
+                    <div class="row1">
+                        <div class="column1" style="background-color:#aaa;">
+                            <h2 class="title">Calculadora</h2>
+                            <a href="https://github.com/sebasramirez07" target="img" rel="noopener noreferrer"> <img class="imgcalc" src="img//Captura de pantalla 2023-04-08 153206.png" alt="Calculadora"></a>
+                        </div>
+                        <div class="column1" style="background-color:#bbb;">
+                            <h2 class="title">Biblioteca de Peliculas</h2>
+                            <a href="https://github.com/sebasramirez07" target="img" rel="noopener noreferrer"> <img class="imgbib" src="img//Captura de pantalla 2023-04-08 153507.png" alt="biblioteca"></a>
+                        </div>
+                        <div class="column1" style="background-color:#ccc;">
+                            <h2 class="title">Biblioteca de Peliculas</h2>
+                            <a href="https://github.com/sebasramirez07" target="img" rel="noopener noreferrer"> <img class="imgpg" src="img//pagina1.jpg" alt="pagina"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </main>
+    <footer id="Contacto" class="footer">
+        <div>
+            <div class="container1">
+                <img src="img//ngt.jpg" alt="Avatar" style="width:90px">
+                <p><span>sebastian Ramirez</span> <br> Estudiante de Programacion en Censa.</p>
+                <div>
+                    <a href="https://www.linkedin.com/in/johan-sebastian-ramirez-palacios-644089108/"><img class="link" src="img//link.png" alt="link"></a>
+                </div>
+                <div>
+                    <a href="https://github.com/sebasramirez07"><img class="git" src="img//git.png" alt="git"></a>
+                </div>
+                <div>
+                    <a href="mailto:Jsebastianramirez5@gamil.com"><img class="gmail" src="img//gmail.png" alt="gmail"></a>
+                </div>
+            </div>
+            <div>
+                <a class="volver" href="#home">Volver al inicio</a>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
